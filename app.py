797d3c84f6570c68a8e6527eb9d8e137021edd9d
Theodor <thgr0003@stud.kea.dk>
2025-05-07 12:19:53 +0200

    Added function that inserts into database

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+from sqlite3 import Connection
 
 app = Flask(__name__)
 
+def addResultsToDatabase(name, age):
+    try:
+        con = Connection("SurveyDatabase.db")
+        cur = con.cursor()
+        params = (name, age)
+        sql = """INSERT INTO Survey1 (name, age) VALUES(?, ?)"""
+        cur.execute(sql, params)
+        cur.commit()
+        con.close()
+    except:
+        print("Failed to connect to database!")
+
 @app.route("/")
 def home():
+    addResultsToDatabase("Theo", 26)
     return render_template("home.html")
 
 @app.route("/Spørgeskema1/")
