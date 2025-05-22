722a56199e900d4a901d45e8f87825fd018b9f25
Theodor <thgr0003@stud.kea.dk>
2025-05-22 13:19:41 +0200

    Minor changes

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def login():
 
         user = Users.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.password, password):
+        if user and match_pass:
             login_user(user)
             return redirect(url_for("admin_dashboard"))
         else:
